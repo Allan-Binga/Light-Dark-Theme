979730eca70a960fe65687d4c877eca0d51ad3c1
Allan-Binga <allanbinga73@gmail.com>
2023-08-29 07:03:00 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DarkMode from './components/Darkmode/DarkMode';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <DarkMode/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
